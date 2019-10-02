c946251b38c8add931f9521cfb6f8494e9e9b09a
nikithsai401 <nikithsai401@users.noreply.github.com>
2019-10-02 22:54:44 +0530

    final stages of arduino
    the db_all_positive is the main file

---
+++ final audipixel/only_voltage_converter/only_voltage_converter.ino
@@ -0,0 +1,100 @@
+// A basic everyday NeoPixel strip test program.
+
+// NEOPIXEL BEST PRACTICES for most reliable operation:
+// - Add 1000 uF CAPACITOR between NeoPixel strip's + and - connections.
+// - MINIMIZE WIRING LENGTH between microcontroller board and first pixel.
+// - NeoPixel strip's DATA-IN should pass through a 300-500 OHM RESISTOR.
+// - AVOID connecting NeoPixels on a LIVE CIRCUIT. If you must, ALWAYS
+//   connect GROUND (-) first, then +, then data.
+// - When using a 3.3V microcontroller with a 5V-powered NeoPixel strip,
+//   a LOGIC-LEVEL CONVERTER on the data line is STRONGLY RECOMMENDED.
+// (Skipping these may work OK on your workbench but can fail in the field)
+
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+ #include <avr/power.h> // Required for 16 MHz Adafruit Trinket
+#endif
+
+// Which pin on the Arduino is connected to the NeoPixels?
+// On a Trinket or Gemma we suggest changing this to 1:
+#define LED_PIN    6
+
+// How many NeoPixels are attached to the Arduino?
+#define LED_COUNT 3
+
+// Declare our NeoPixel strip object:
+Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+// Argument 1 = Number of pixels in NeoPixel strip
+// Argument 2 = Arduino pin number (most are valid)
+// Argument 3 = Pixel type flags, add together as needed:
+//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
+//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
+//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
+//   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
+
+int sensorPin=A1;
+void setup()
+{
+  pinMode(sensorPin, INPUT);
+  Serial.begin (9600);
+  
+  #if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
+  clock_prescale_set(clock_div_1);
+#endif
+  // END of Trinket-specific code.
+
+  strip.begin();           // INITIALIZE NeoPixel strip object (REQUIRED)
+  strip.show();            // Turn OFF all pixels ASAP
+  strip.setBrightness(255); // Set BRIGHTNESS to about 1/5 (max = 255)
+ // strip.setPixelColor(1,255,255,255);
+  //strip.show(); 
+}
+
+
+
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+int val;
+ val =analogRead(sensorPin);
+ Serial.println (val);
+ //delay(20);
+ if(val>480 &&val<535) //mean = 509
+ {
+   val=val-val;
+ }
+ else{
+   val = 1;
+ }
+ 
+   //Serial.println (val);
+  //delay(15);
+
+if(val==1)
+{
+  for(int n=0;n<strip.numPixels();n++){
+  strip.setPixelColor(n,255,0,0);
+  }
+  strip.show();
+  delay(30);
+  //strip.setBrightness(255);
+  Serial.println ("j0 pressed");
+  
+}
+else
+{
+  //strip.show();
+  for(int n=0;n<strip.numPixels();n++){
+  strip.setPixelColor(n,255,0,0);
+  }
+  strip.show();
+  //delay(30);
+  Serial.println ("j1 pressed");
+  //strip.setBrightness(100);
+  
+  
+}
+
+}
